c9709e829c02226642bd28b6e361609cf223b9d7
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-05-10 14:45:14 +0700

    fix

--- src/app/(dashboard)/asuransi/[no_msn]/asuransi-detail.jsx
+++ src/app/(dashboard)/asuransi/[no_msn]/asuransi-detail.jsx
@@ -131,6 +131,17 @@ export default function AsuransiDetailPage({asuransi}){
                 <label
                     htmlFor="kode-kerja"
                     className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                    Tanggal Faktur
+                </label>    
+                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                    <input className="max-w-lg pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200 cursor-pointer" 
+                    type="date" name="nm_mtr" id="" value={formData.tgl_faktur.slice(0,10)}/>
+                </div>
+            </div>
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                    htmlFor="kode-kerja"
+                    className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                     Nomor Telepon
                 </label>    
                 <div className="mt-1 sm:mt-0 sm:col-span-2">
