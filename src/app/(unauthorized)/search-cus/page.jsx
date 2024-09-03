a7696dabf148ef581843eda39e8d43fb8dc4eaad
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-09-03 10:43:09 +0700

    fix

--- src/app/(unauthorized)/search-cus/page.jsx
+++ src/app/(unauthorized)/search-cus/page.jsx
@@ -14,6 +14,8 @@ export default function Page() {
   const { register, handleSubmit } = useForm();
   const [data, setdata] = useState([]);
   const [loading, setloading] = useState(false);
+  const [noHp, setnoHp] = useState("")
+  
 
   const onSubmit = async (values) => {
     setloading(true);
@@ -58,17 +60,22 @@ export default function Page() {
             <label htmlFor="no_hp" className="block text-sm font-medium leading-6 text-gray-900">
               Nomor Telepon
             </label>
-            <div className="mt-2">
+            <div className="mt-2 relative">
+              <p className="absolute left-2 top-1">+628</p>
               <input
                 id="no_hp"
                 name="no_hp"
-                type="number"
+                value={noHp}
+                onChange={(e)=>{
+                  let a = e.target.value
+                  if ( /^[0-9]*$/.test(a)) {
+                    setnoHp(a)
+                  }
+                }}
+                type="tel"
                 autoComplete="no_hp"
-                {...register("no_hp", {
-                  required: "This field is required",
-                })}
                 required
-                className="block w-full appearance-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full pl-[55px] appearance-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -78,7 +85,7 @@ export default function Page() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Find
             </button>
           </div>
         </form>
